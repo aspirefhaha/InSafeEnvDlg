ad65d969cad3a78daca833cb4485210ba7ce0dc7
hongliang <hongliang@yuanqirongan.com>
2020-03-11 11:17:02 +0800

    Build at Out ok

---
+++ README.md
@@ -0,0 +1,11 @@
+# Build Method
+
+## in windows
+
+```
+set PATH=D:\VBox\qt-4.7.3\bin;%PATH%;
+lupdate -pro InSaveEnvDlg.pro
+lrelease zh_CN.ts
+```
+
+now could build in vs2010
--- zh_CN.ts
+++ zh_CN.ts
@@ -4,115 +4,118 @@
 <context>
     <name>InSaveEnvDlg</name>
     <message>
-        <location filename="insaveenvdlg.cpp" line="72"/>
+        <location filename="insaveenvdlg.cpp" line="163"/>
+        <location filename="insaveenvdlg.cpp" line="204"/>
         <source>Target Count Err</source>
         <translation type="unfinished">目标数量错误</translation>
     </message>
     <message>
-        <location filename="insaveenvdlg.cpp" line="72"/>
+        <location filename="insaveenvdlg.cpp" line="163"/>
+        <location filename="insaveenvdlg.cpp" line="204"/>
         <source>multi target dir or nothing is choosed,pls select a singe dir as target</source>
         <translation type="unfinished">目标选择了多个位置或者未选择，请只选择一个文件夹</translation>
     </message>
     <message>
-        <location filename="insaveenvdlg.cpp" line="79"/>
+        <location filename="insaveenvdlg.cpp" line="170"/>
+        <location filename="insaveenvdlg.cpp" line="212"/>
         <source>Target Type Err</source>
         <translation type="unfinished">目标类型错误</translation>
     </message>
     <message>
-        <location filename="insaveenvdlg.cpp" line="79"/>
+        <location filename="insaveenvdlg.cpp" line="170"/>
+        <location filename="insaveenvdlg.cpp" line="212"/>
         <source>Target is Not Dir</source>
         <translation type="unfinished">目标不是文件夹</translation>
+    </message>
+    <message>
+        <location filename="insaveenvdlg.cpp" line="177"/>
+        <location filename="insaveenvdlg.cpp" line="219"/>
+        <source>Source Count Err</source>
+        <translation type="unfinished">源数量错误</translation>
+    </message>
+    <message>
+        <location filename="insaveenvdlg.cpp" line="177"/>
+        <location filename="insaveenvdlg.cpp" line="219"/>
+        <source>no source dir or file is choosed,pls select source</source>
+        <translation type="unfinished">没有选择源文件夹或文件</translation>
     </message>
 </context>
 <context>
     <name>InSaveEnvDlgClass</name>
     <message>
         <location filename="insaveenvdlg.ui" line="17"/>
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="177"/>
         <source>InSaveEnvDlg</source>
         <translation type="unfinished">环境内资源拷贝应用</translation>
-    </message>
-    <message utf8="true">
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="178"/>
-        <source>环境外</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="29"/>
         <source>OutEnv</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">外部环境</translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="56"/>
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="179"/>
         <source>&lt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">《</translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="79"/>
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="180"/>
         <source>&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">》</translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="103"/>
         <source>InEnv</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">环境内</translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="122"/>
         <source>OutSelected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">外部选择</translation>
     </message>
     <message>
         <location filename="insaveenvdlg.ui" line="136"/>
         <source>InSelected</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message utf8="true">
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="181"/>
-        <source>环境内</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message utf8="true">
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="182"/>
-        <source>环境外选择</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message utf8="true">
-        <location filename="GeneratedFiles/ui_insaveenvdlg.h" line="184"/>
-        <source>环境内选择</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">内部选择</translation>
     </message>
 </context>
 <context>
     <name>QOutEnvFSModel</name>
     <message>
-        <location filename="qoutenvfsmodel.cpp" line="44"/>
+        <location filename="qoutenvfsmodel.cpp" line="606"/>
         <source>Name</source>
         <translation type="unfinished">名称</translation>
     </message>
     <message>
-        <location filename="qoutenvfsmodel.cpp" line="46"/>
+        <location filename="qoutenvfsmodel.cpp" line="608"/>
         <source>Size</source>
         <translation type="unfinished">大小</translation>
     </message>
     <message>
-        <location filename="qoutenvfsmodel.cpp" line="50"/>
+        <location filename="qoutenvfsmodel.cpp" line="612"/>
         <source>Kind</source>
         <comment>Match OS X Finder</comment>
         <translation type="unfinished">类型</translation>
     </message>
     <message>
-        <location filename="qoutenvfsmodel.cpp" line="52"/>
+        <location filename="qoutenvfsmodel.cpp" line="614"/>
         <source>Type</source>
         <comment>All other platforms</comment>
         <translation type="unfinished">类型</translation>
     </message>
     <message>
-        <location filename="qoutenvfsmodel.cpp" line="59"/>
+        <location filename="qoutenvfsmodel.cpp" line="621"/>
         <source>Date Modified</source>
         <translation type="unfinished">修改时间</translation>
+    </message>
+    <message>
+        <location filename="qoutenvfsmodel.cpp" line="757"/>
+        <source>File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="qoutenvfsmodel.cpp" line="757"/>
+        <source>File Folder</source>
+        <translation type="unfinished">文件夹</translation>
     </message>
 </context>
 </TS>
